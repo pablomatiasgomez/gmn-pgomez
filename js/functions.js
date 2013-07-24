ed2d406ed11529a4621ce596cfa31423b3241ba3
Pablo Gomez <pablomatiasgomez@gmail.com>
2013-07-24 12:56:22 -0300

    Cambios minimos en js

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,36 @@
+////////////////////////
+// Gomez Pablo Matias //
+//                    //
+////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 $(document).ready(function() { $("#divServer").show(); });
 
 $(function(){ //ready function 
 
-    $(".initForm input[type='text']").keypress(function (event) { if (event.keyCode == 13)  $($($(this).parent()).find("input[type='button']")).click(); });
+    $(".initForm input[type='text']").keyup(function (event) { $($($(this).parent()).find("input[type='button']")).click();  });
     
     $("#btnServer_Save").click(function (){
+        alert($.md5("1234"));
+//  10000000000000
+//  99999999999999999
+//0.77453316655009981
+
+
+        return;
         flag = true;
         if (parseInt($("#txtPort").val(), 10) != $("#txtPort").val()) {
             validateControl($("#txtPort"), false);
@@ -16,7 +40,7 @@ $(function(){ //ready function
             validateControl($("#txtServer"), false);
             flag = false;
         } else validateControl($("#txtServer"), true);
-        if (flag){
+        if (flag && event.keyCode === 13){
             $("#divServer").fadeOut(500, function(){ $("#divUser").fadeIn(500, function(){ $("#txtUser").focus() })});
         }
     });
@@ -27,7 +51,7 @@ $(function(){ //ready function
         if (($("#txtUser").val()).length < 1)
             validateControl($("#txtUser"), false);
         else 
-            $("#divUser").fadeOut(500, function(){ $("#divNumber").fadeIn(500, function(){ $("#txtNumber").focus() })});
+            if (event.keyCode === 13) $("#divUser").fadeOut(500, function(){ $("#divNumber").fadeIn(500, function(){ $("#txtNumber").focus() })});
    
     });
     
@@ -35,7 +59,7 @@ $(function(){ //ready function
         if ((parseInt($("#txtNumber").val(), 10) != $("#txtNumber").val()) || ($("#txtNumber").val()).length != 4 )
             validateControl($("#txtNumber"), false);
         else
-            $("#divNumber").fadeOut(500, function(){ $("#divMain").fadeIn(500, function(){})});
+            if (event.keyCode === 13) $("#divNumber").fadeOut(500, function(){ $("#divMain").fadeIn(500, function(){})});
     });
 });
 
@@ -44,7 +68,7 @@ $(function(){ //ready function
 
 function validateControl(control, valid) {
     if (valid)
-        $(control).animate({ backgroundColor: '#C0FFC0' }, 500);
+        $(control).animate({ backgroundColor: '#C0FFC0' }, 300);
     else
-        $(control).animate({ backgroundColor: '#FFD0D0' }, 500);
+        $(control).animate({ backgroundColor: '#FFD0D0' }, 300);
 }

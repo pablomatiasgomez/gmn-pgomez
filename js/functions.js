44627a404319b4e7a361f8612c3bdda8ec114546
Pablo Gomez <pablomatiasgomez@gmail.com>
2013-07-26 08:09:26 -0300

    Cambios js

--- js/functions.js
+++ js/functions.js
@@ -4,28 +4,7 @@
 ////////////////////////
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function() { $("#divServer").show(); });
-
-$(function(){ //ready function 
-    
-    
-    $(".initForm input[type='text']").keyup(function (event) { $($($(this).parent()).find("input[type='button']")).click();  });
-    
-    $("#btnServer_Save").click(function (){
-        /*for(x=10000000000000; x <= 99999999999999999; x++){
+/*for(x=10000000000000; x <= 99999999999999999; x++){
             if ($.md5(1234 + x) == "cd579ee4ac1487196859e3799a0c9861")
                 alert (x);
             if ((x % 10000000) == 0)
@@ -39,47 +18,69 @@ $(function(){ //ready function
 
 
         return;*/
-        flag = true;
-        if (parseInt($("#txtPort").val(), 10) != $("#txtPort").val()) {
-            validateControl($("#txtPort"), false);
-            flag = false;
-        } else validateControl($("#txtPort"), true);
-        if (($("#txtServer").val()).length < 1){
-            validateControl($("#txtServer"), false);
-            flag = false;
-        } else validateControl($("#txtServer"), true);
-        if (flag) 
-            $("#frmServer").animate({ borderColor: '#00FF00'}, 300 );
-        else
-            $("#frmServer").animate({ borderColor: '#FF0000'}, 300 );
-            
-        if (flag && event.keyCode === 13){
+
+$(document).ready(function() { $("#divServer").show(); });
+
+$(function(){ //ready function 
+
+    $("#frmServer input[type='text']").keyup(function (event) { if (event.keyCode === 13) $($($(this).parent()).find("input[type='button']")).click(); else validateForm(1); });
+    $("#frmUser input[type='text']").keyup(function (event) { if (event.keyCode === 13) $($($(this).parent()).find("input[type='button']")).click(); else validateForm(2); });
+    $("#frmNumber input[type='text']").keyup(function (event) { if (event.keyCode === 13) $($($(this).parent()).find("input[type='button']")).click(); else validateForm(3); });
+    
+    
+    $("#btnServer_Save").click(function (){
+        if (validateForm(1))
             $("#divServer").fadeOut(500, function(){ $("#divUser").fadeIn(500, function(){ $("#txtUser").focus(); })});
-        }
     });
     
-    
     $("#btnUser_Save").click(function (){
-        
-        if (($("#txtUser").val()).length < 1)
-            validateControl($("#txtUser"), false);
-        else 
-            if (event.keyCode === 13) $("#divUser").fadeOut(500, function(){ $("#divNumber").fadeIn(500, function(){ $("#txtNumber").focus(); })});
-   
+        if (validateForm(2))
+            $("#divUser").fadeOut(500, function(){ $("#divNumber").fadeIn(500, function(){ $("#txtNumber").focus(); })});
     });
     
     $("#btnNumber_Save").click(function (){
-        if ((parseInt($("#txtNumber").val(), 10) != $("#txtNumber").val()) || ($("#txtNumber").val()).length != 4 )
-            validateControl($("#txtNumber"), false);
-        else
-            if (event.keyCode === 13) $("#divNumber").fadeOut(500, function(){ $("#divMain").fadeIn(500, function(){})});
+        if (validateForm(3))
+            $("#divNumber").fadeOut(500, function(){ $("#divMain").fadeIn(500, function(){})});
     });
-    
     $("#txtServer").focus();
 });
 
 
-
+function validateForm(form){
+    flag = true;
+    if (form === 1){
+        if (parseInt($("#txtPort").val(), 10) != $("#txtPort").val()) {
+            validateControl($("#txtPort"), false);
+            flag = false;
+        } else validateControl($("#txtPort"), true);
+        
+        if (($("#txtServer").val()).length < 1){
+            validateControl($("#txtServer"), false);
+            flag = false;
+        } else validateControl($("#txtServer"), true);
+        
+        if (flag) 
+            $("#frmServer").animate({ 'boxShadow': '0 0 10px 2px #00FF00', borderColor: '#00FF00'  } , 300 );
+        else
+            $("#frmServer").animate({ borderColor: '#FF0000',  boxShadow : '0px 0px 10px 2px #FF0000'}, 300 );
+        
+    }
+    else if (form === 2){
+        if (($("#txtUser").val()).length < 1) {
+            validateControl($("#txtUser"), false);
+            flag = false;
+        }
+        else validateControl($("#txtUser"), true);
+    }
+    else if (form === 3){
+        if ((parseInt($("#txtNumber").val(), 10) != $("#txtNumber").val()) || ($("#txtNumber").val()).length != 4) {
+            validateControl($("#txtNumber"), false);
+            flag = false;
+        }
+        else validateControl($("#txtNumber"), true);
+    }
+    return flag;
+}
 
 function validateControl(control, valid) {
     if (valid)

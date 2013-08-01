07684eb7be4699e591bc639578e3a8ea8055850d
Pablo Gomez <pablomatiasgomez@gmail.com>
2013-08-01 01:20:45 -0300

    Intentando hacer primer version del BOT...

---
+++ BOT/js/server.js
@@ -0,0 +1,31 @@
+var Server = function() {
+    //variables privadas
+    var url = "";
+
+    var setUrl = function(value) {
+        url = value;
+    }
+    
+    getData = function(subUrl, oClient, callback){
+        subUrl = subUrl.replace("<privateUuid>", oClient.getPrivateID());
+        subUrl = subUrl.replace("<publicUuid>", oClient.getGuessingToID());
+
+        $.ajax({
+            async: false,
+            type: 'GET',
+            url: url + subUrl,
+            dataType:"json",
+            success: function(data) {
+                callback(data);
+            },
+            error: function(data) {
+                callback(data);
+            }
+        });
+    }
+        
+    return {
+        "setUrl": setUrl,
+        "getData": getData,
+    }
+};

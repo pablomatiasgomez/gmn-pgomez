02f3a1de869da450be1de7313f6f08878cb32095
Pablo Gomez <pablomatiasgomez@gmail.com>
2013-08-03 15:59:57 -0300

    Ordenamiento general de codigo y mejoras generales

--- js/server.js
+++ js/server.js
@@ -1,7 +1,6 @@
 var Server = function() {
     //variables privadas
     var url = "";
-    var waiting = false;
     
     var setUrl = function(value) {
         url = value;
@@ -10,25 +9,17 @@ var Server = function() {
     getData = function(subUrl, oClient, callback){
         subUrl = subUrl.replace("<privateUuid>", oClient.getPrivateID());
         subUrl = subUrl.replace("<publicUuid>", oClient.getGuessingToID());
-        waiting = true;
+
         $.ajax({
             type: 'GET',
             url: url + subUrl,
             dataType:"json",
-           /* success: function(data) {
-                // TODO: USAR COMPLETE
-                callback(data);
-            },
-            error: function(data) {
-                callback(data);
-            }*/
             complete: function(data) {
-                // TODO: USAR COMPLETE
                 callback(data);
             },
-        }).done (function() { waiting = false;});
+        })
     }
-        
+  
     return {
         "setUrl": setUrl,
         "getData": getData,
